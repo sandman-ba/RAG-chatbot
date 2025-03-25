cde0ca3adf36d60b47baf175875ce0f1fb208cc1
vinny <vjodoin1@outlook.com>
2025-03-25 13:01:53 -0400

    mistral switch

--- save-pretrained-model.py
+++ save-pretrained-model.py
@@ -1,6 +1,10 @@
 from transformers import BloomTokenizerFast, BloomModel
 
-tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom", force_download=True)
+model_id = "meta-llama/Meta-Llama-3-8B"
+# model_id = "igscience/bloom-1b7"
+
+
+tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-1b7", force_download=True)
 model = BloomModel.from_pretrained("bigscience/bloom", force_download=True)
 
 tokenizer.save_pretrained("./tokenizers/bloom")
